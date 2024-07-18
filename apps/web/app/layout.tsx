7ee1a6474a8ed9b9607d290e770301ec04a13ac6
Martin Miklik <martin.miklik@brandmaster.com>
2024-07-18 23:45:20 +0200

    chore: Update package dependencies and add new components

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+import { ThemeProvider, Navbar } from 'components'
 import '@repo/ui/globals.css'
 import './globals.css'
 
@@ -25,7 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
